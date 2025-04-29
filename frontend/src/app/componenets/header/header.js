6fd0b3f16d9e98965a0bcca9e4e06e8a172960eb
Colin115 <89537192+Colin115@users.noreply.github.com>
2025-04-29 18:16:48 -0500

    added header

---
+++ frontend/src/app/componenets/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <span>arc</span>
+      <h1>Title</h1>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/app/logIn/page.js
+++ frontend/src/app/logIn/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./componenets/header"
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Header />
+      Sign In
     </div>
   );
 }
