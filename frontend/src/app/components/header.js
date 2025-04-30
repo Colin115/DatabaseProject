f97a3d1c2d3698b9407f3dd1a5cb41c123bf1e53
Rohit <rsbby@umsystem.edu>
2025-04-30 12:29:52 -0500

    rearranged titlepage code to page.js in public directory as opposed to components directory. also changed some css code related to the header and updated header to be a simple one-liner for now

--- frontend/src/app/components/header.js
+++ frontend/src/app/components/header.js
@@ -4,21 +4,7 @@ import styles from "./header.module.css";
 const Header = () => {
   return (
     <div className = {styles.main}>
-      <div className = {styles.submain}>
-        <h1 className = {styles.title}><center>Nest</center></h1>
-        <span className = {styles.subtitle}><center>By A.R.C.</center></span>
-        <div className = {styles.button}>
-          <center>
-            <button className={styles.button1}>Login</button> 
-            <button className={styles.button2}>Sign Up</button>
-          </center>
-          
-        </div>
-
-        <h2 className = {styles.about}>About Us</h2>
-        <p className = {styles.aboutText}>Nest is an application dedicated to helping you with your job search! Ever need a place to store information? Look no further. With our free and easy to use application, you can store your job search details with us and will store it securely and efficiently with our added database components. We thank you for choosing Nest!</p>
-
-      </div>
+      <h2 className = {styles.titleHeader}>Nest v.0.1 - An application made by A.R.C.</h2>
     </div>
   );
 };
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,6 +6,21 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
+      <div className = {styles.submain}>
+        <h1 className = {styles.title}><center>Nest</center></h1>
+        <span className = {styles.subtitle}><center>By A.R.C.</center></span>
+        <div className = {styles.button}>
+          <center>
+            <button className={styles.button1}>Login</button> 
+            <button className={styles.button2}>Sign Up</button>
+          </center>
+          
+        </div>
+
+        <h2 className = {styles.about}>About Us</h2>
+        <p className = {styles.aboutText}>Nest is an application dedicated to helping you with your job search! Ever need a place to store information? Look no further. With our free and easy to use application, you can store your job search details with us and will store it securely and efficiently with our added database components. We thank you for choosing Nest!</p>
+
+      </div>
     </div>
   );
 }
