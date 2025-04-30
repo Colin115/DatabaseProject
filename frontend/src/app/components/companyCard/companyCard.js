5c7b28ba277c00c75ea786ca9dbd67ce8049cd47
Colin115 <89537192+Colin115@users.noreply.github.com>
2025-04-29 19:07:44 -0500

    added base layout for componenets

---
+++ frontend/src/app/components/companyCard/companyCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CopmanyCard = () => {
+  return (
+    <div>
+      <h1><center>Title</center></h1>
+      <span><center>By A.R.C.</center></span>
+    </div>
+  );
+};
+
+export default CopmanyCard;
---
+++ frontend/src/app/components/resumeCard/resumeCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ResumeCards = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default ResumeCards;
