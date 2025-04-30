4d6bfb6e7c503d4f8ecd399d18aaa283d673f7e0
Colin115 <89537192+Colin115@users.noreply.github.com>
2025-04-29 19:04:12 -0500

    added sign up

--- frontend/src/app/signUp/page.js
+++ frontend/src/app/signUp/page.js
@@ -1,10 +1,48 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "../components/header";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <div className={styles.page}>
-      Sign In
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Create an Account</h1>
+          <form className={styles.form}>
+            <article className={styles.formItem}>
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" id="firstName" placeholder="John" />
+            </article>
+            <article className={styles.formItem}>
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id="lastName" placeholder="Doe" />
+            </article>
+            <article className={styles.formItem}>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" placeholder="johndoe123" />
+            </article>
+            <article className={styles.formItem}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="you@example.com" />
+            </article>
+            <article className={styles.formItem}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Create a password"
+              />
+            </article>
+
+            <button className={styles.button} type="submit">
+              Sign Up
+            </button>
+          </form>
+          <p className={styles.signup}>
+            Already have an account? <a href="/">Log in</a>
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
