d63b00c5b6a9386e78f1c15138cf143cc67c3e15
Rohit <rsbby@umsystem.edu>
2025-04-29 19:00:47 -0500

    Updated title screen

--- frontend/src/app/components/header.js
+++ frontend/src/app/components/header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
     <div>
-      <h1><center>Title</center></h1>
-      <span><center>By A.R.C.</center></span>
+      <h1 className = {styles.title}><center>Nest</center></h1>
+      <span className = {styles.subtitle}><center>By A.R.C.</center></span>
+      <div className = {styles.button}>
+      <button className={styles.button1}>Login</button>
+      <button className={styles.button2}>Sign Up</button>
+      </div>
     </div>
   );
 };
